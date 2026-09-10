e821ebc6b3d63ad9aeb1ab02ca7d72c2475248f7
Alex Kimari <officialalex236@gmail.com>
2026-09-10 12:11:17 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+import path from "path";
+const __dirname = import.meta.dirname;
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@ui": path.resolve(__dirname, "./src/components/ui"),
+    },
+  },
+});
