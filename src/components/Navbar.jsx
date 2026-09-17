c2a277da7e4c17983e797a1d61e7cfffe6ad10c9
Alex Kimari <officialalex236@gmail.com>
2026-09-17 20:49:48 +0300

    Added responsive and dock control

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import { Button } from "@ui/button";
-import { Plus } from "lucide-react";
+import { useMediaQuery } from "react-responsive";
+import { cn } from "cn";
 import {
   ForkKnifeCrossed,
   Map, Podium,
-  Sparkles, Share2
+  Sparkles, Share2,
+  Plus
 } from "lucide-react";
+
+
+const Nav = ({ className, spanClassName }) => {
+  return (
+    <nav className={cn(className)}>
+      <span className={cn(spanClassName)}>
+        <Map className="badge-icon" />
+        Map
+      </span>
+      <span className={cn(spanClassName)}>
+        <Podium className="badge-icon" />
+        Rank
+      </span>
+      <span className={cn(spanClassName)}>
+        <Sparkles className="badge-icon" />
+        Recap
+      </span>
+      <span className={cn(spanClassName)}>
+        <Share2 className="badge-icon" />
+        Share
+      </span>
+    </nav>
+  );
+}
 
 
 export default function Navbar () {
@@ -15,65 +41,51 @@ export default function Navbar () {
   const goHome = () => {
     navigate("/")
   }
+
+  const enableDock = useMediaQuery({
+    query: "(max-width: 768px)"
+  })
   
   
   return (
-    <header>
-      <div
-        className="logo cursor-pointer"
-        onClick={
-          () => goHome()
-        }
-      >
-        <Button className="btn">
-          <ForkKnifeCrossed />
-        </Button>
-        <h1>
-          Foodie
-          <span>.</span>
-        </h1>
-      </div>
-
-      <nav>
-        <span>
-          <Map className="badge-icon" />
-          Map
-        </span>
-
-        <span>
-          <Podium className="badge-icon" />
-          Rank
-        </span>
-
-        <span>
-          <Sparkles className="badge-icon" />
-          Recap
-        </span>
-
-        <span>
-          <Share2 className="badge-icon" />
-          Share
-        </span>
-
-        <Button
-          className="btn max-[640px]:rounded-sm"
+    <>
+      <header>
+        <div
+          className="logo cursor-pointer"
+          onClick={
+            () => goHome()
+          }
         >
-          <Plus className="text-muted" />
-          <span
-            className="max-[640px]:hidden"
-          >
-            Add a place
-          </span>
-        </Button>
+          <Button className="btn">
+            <ForkKnifeCrossed />
+          </Button>
+          <h1>
+            Foodie
+            <span>.</span>
+          </h1>
+        </div>
+        <div className="nav-elements">
+          {
+            !enableDock && <Nav />
+          }
 
-        <ThemeToggle />
+          <Button className="btn max-[640px]:rounded-sm">
+            <Plus className="text-muted" />
+            <span className="max-[640px]:hidden">Add a place</span>
+          </Button>
+          <ThemeToggle />
+          <Button className="avatar">AK</Button>
+        </div>
+      </header>
 
-        <Button
-          className="avatar"
-        >
-          AK
-        </Button>
-      </nav>
-    </header>
+      {
+        enableDock
+          && 
+            <Nav
+              className="absolute bottom-0 left-0 right-0 flex justify-around items-center p-3 px-6 border-t"
+              spanClassName="cursor-pointer flex flex-col items-center"
+            />
+      }
+    </>
   );
 }
